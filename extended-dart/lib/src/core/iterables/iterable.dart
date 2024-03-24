15bf70ee492120655eb42ecff92b36a4c749289d
nikechukwu <nikechukwu@googlemail.com>
2024-03-24 00:01:31 +0000

    Referenced Extension Code

--- extended-dart/lib/src/core/iterables/iterable.dart
+++ extended-dart/lib/src/core/iterables/iterable.dart
@@ -1,8 +1,11 @@
-extension <T> on Iterable<T> {
-  Iterable<T> applyWhere(bool Function(T element) test, T Function(T element) apply) {
+extension IterbaleExtensions<T> on Iterable<T> {
+  Iterable<T> applyWhere(
+      bool Function(T element) test, T Function(T element) apply) {
     return this.map((e) {
-      if (test(e)) return apply(e);
-      else return e;
+      if (test(e))
+        return apply(e);
+      else
+        return e;
     });
   }
 
@@ -15,15 +18,19 @@ extension NullCheck<T> on Iterable<T?> {
   int countNull() => this.where((element) => element == null).length;
 }
 
-extension on Iterable<String> {
+extension StringIterableExtensions on Iterable<String> {
   Iterable<String> splitMapJoin(Pattern separator, {int? start, int? end}) {
-    return this.toList()
-    .getRange(start ?? 0, end ?? this.length)
-    .map((e) => e.split(separator))
-    .fold([], (previousValue, element) => previousValue.followedBy(element));
+    return this
+        .toList()
+        .getRange(start ?? 0, end ?? this.length)
+        .map((e) => e.split(separator))
+        .fold(
+            [], (previousValue, element) => previousValue.followedBy(element));
   }
 
-  Iterable<String> splitMapJoinWhere(Pattern separator, bool Function(String element) test, {int? start, int? end}) {
+  Iterable<String> splitMapJoinWhere(
+      Pattern separator, bool Function(String element) test,
+      {int? start, int? end}) {
     return this;
   }
 }
