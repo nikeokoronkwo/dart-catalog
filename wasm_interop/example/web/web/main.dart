2b5d492289402eade58956b0f0c610ab7a98a515
nikechukwu <nikechukwu@googlemail.com>
2024-04-06 00:55:09 +0000

    Set Up WASM Example Working

--- wasm_interop/example/web/web/main.dart
+++ wasm_interop/example/web/web/main.dart
@@ -1,8 +1,16 @@
-import 'package:web/helpers.dart';
+import 'dart:js_interop';
+
+import 'package:web/web.dart';
+import 'package:wai/wai.dart';
 
 void main() {
   final now = DateTime.now();
   final element = document.querySelector('#output') as HTMLDivElement;
   element.text = 'The time is ${now.hour}:${now.minute}'
       ' and your Dart web app is running!';
+  WebAssembly.compileStreaming(fetch('wasm/main.wasm')).then((value) async {
+    return await WebAssembly.instantiate(value);
+  }).then((instance) {
+    print((instance as WebAssemblyInstance).exports['sumofsquares'](9));
+  });
 }
---
+++ wasm_interop/example/web/web/wasm/main.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+
+int sumofnums(int n) {
+    return (n*(n+1))/2;
+}
+
+int sumofsquares(int n) {
+    return (n*(n+1)*(2*n+1))/6;
+}
