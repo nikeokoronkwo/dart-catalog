939c557ca5cd71718b74b6718b42dec631bb10ad
nikechukwu <nikechukwu@googlemail.com>
2024-04-06 00:56:00 +0000

    Made Changes to Code

--- extended-dart/lib/src/web/helpers/json.dart
+++ extended-dart/lib/src/web/helpers/json.dart
@@ -1,7 +1,7 @@
 import 'dart:js_interop';
 
 @JS('JSON.stringify')
-external String stringify(dynamic value);
+external String stringify(JSObject value);
 
 @JS('JSON.parse')
 external JSObject parse(String text);
