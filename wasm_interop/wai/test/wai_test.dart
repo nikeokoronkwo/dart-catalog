c7693417fdf0b7a5a55988ffccb09e2f21d3e315
nikechukwu <nikechukwu@googlemail.com>
2024-04-06 03:20:39 +0000

    Test Placeholders

--- wasm_interop/wai/test/wai_test.dart
+++ wasm_interop/wai/test/wai_test.dart
@@ -1,5 +1,12 @@
+@TestOn('browser')
+
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {});
+  group('A group of tests', () {
+    test('wai test', () {
+      // Nothing Here For Now
+      return null;
+    });
+  });
 }
