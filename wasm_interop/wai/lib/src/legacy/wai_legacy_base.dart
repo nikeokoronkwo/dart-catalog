1b6cf62d9613598e885efe2996a7b4abb9b3b01b
nikechukwu <nikechukwu@googlemail.com>
2024-04-06 00:57:16 +0000

    Minor Fixes

--- wasm_interop/wai/lib/src/legacy/wai_legacy_base.dart
+++ wasm_interop/wai/lib/src/legacy/wai_legacy_base.dart
@@ -2,9 +2,11 @@ import 'package:js/js.dart';
 
 @JS('WebAssembly')
 @staticInterop
-class WebAssembly {}
-
-extension WebAssemblyDeclarations on WebAssembly {
+class WebAssembly {
   external static instantiateStreaming(Object wasm, Object importObject);
   external static instantiate(Object wasmBuffer, Object importObject);
+}
+
+extension WebAssemblyDeclarations on WebAssembly {
+
 }
